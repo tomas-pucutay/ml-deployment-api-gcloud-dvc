a62710205cc8777fa5f49323e65939eb0de78760
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-18 17:09:46 -0500

    Prepare.py finished: Aggregates data and export csv

--- src/prepare.py
+++ src/prepare.py
@@ -22,4 +22,16 @@ fin_data = pd.read_csv(StringIO(finantial_data_path))
 movie_data = pd.read_csv(StringIO(movie_data_path))
 opening_data = pd.read_csv(StringIO(opening_data_path))
 
-breakpoint()
+numeric_columns_mask = (movie_data.dtypes == float) | (movie_data.dtypes == int)
+numeric_columns = [column for column in numeric_columns_mask.index if numeric_columns_mask[column]]
+movie_data = movie_data[numeric_columns+['movie_title']]
+
+fin_data = fin_data[['movie_title','production_budget','worldwide_gross']]
+
+fin_movie_data = pd.merge(fin_data, movie_data, on="movie_title", how="left")
+full_movie_data = pd.merge(opening_data, fin_movie_data, on="movie_title", how="left")
+
+full_movie_data = full_movie_data.drop(["gross","movie_title"], axis=1)
+full_movie_data.to_csv('dataset/full_data.csv', index=False)
+
+logger.info("Data fetched and prepared...")
